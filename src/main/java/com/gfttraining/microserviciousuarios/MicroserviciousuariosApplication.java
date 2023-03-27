a15e2fa1a569bc49c61b6192b070f4c894f4e899
Sergi Perez <sergi.perez@gft.com>
2023-03-27 10:45:10 +0000

    proyecto añadido

---
+++ src/main/java/com/gfttraining/microserviciousuarios/MicroserviciousuariosApplication.java
@@ -0,0 +1,13 @@
+package com.gfttraining.microserviciousuarios;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroserviciousuariosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroserviciousuariosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gfttraining/microserviciousuarios/MicroserviciousuariosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gfttraining.microserviciousuarios;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroserviciousuariosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
